99d4a4a2d3da3155099bbc7218aefd40c7be1b96
fadlullan <fadlullahherifarid@gmail.com>
2024-08-12 02:46:37 +0800

    revisi

--- app/src/main/java/com/example/yolov8tflite/Constants.kt
+++ app/src/main/java/com/example/yolov8tflite/Constants.kt
@@ -1,6 +1,6 @@
 package com.example.yolov8tflite
 
 object Constants {
-    const val MODEL_PATH = "best_revisi_1_float32.tflite"
+    const val MODEL_PATH = "best_float32.tflite"
     const val LABELS_PATH = "emosi.txt"
 }
